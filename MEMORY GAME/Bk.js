a1f0dc9c54b01586570da07c7d113efe53871810
BoART96 <45164766+BoART96@users.noreply.github.com>
2019-03-26 11:06:28 +0200

    Update Bk.js
    Added comments for clearer understanding of my code by the reader.

--- MEMORY GAME/Bk.js
+++ MEMORY GAME/Bk.js
@@ -1,15 +1,16 @@
 const cards = document.querySelectorAll('.Card');
 
 let hasFlippedCard = false;
-let lockBoard = false;
+let lockBoard = false; //To stop the user from picking more than 2 cards
 let firstCard, secondCard;
 
+//This is where the game starts
 function flipCard() {
   if (lockBoard) return;
-  if (this === firstCard) return;
-
+  if (this === firstCard) return;//prevents the user from double clicking on the same card
   this.classList.add('flip');
 
+ //Stops the user from flipping the same card twice
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
@@ -18,6 +19,7 @@ function flipCard() {
   }
 
   secondCard = this;
+
   checkForMatch();
 }
 
@@ -27,6 +29,7 @@ function checkForMatch() {
   isMatch ? disableCards() : unflipCards();
 }
 
+//Prevent any interaction after the cards match
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
@@ -34,6 +37,7 @@ function disableCards() {
   resetBoard();
 }
 
+//To return the cards back around after there is no match
 function unflipCards() {
   lockBoard = true;
 
@@ -45,11 +49,13 @@ function unflipCards() {
   }, 700);
 }
 
+//To make sure that not more than 2 cards are chosen
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
 
+//To randomise the positions of the cards on the board  --> Learnt this syntax on ES6(Call right after creating function)
 (function shuffle() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
